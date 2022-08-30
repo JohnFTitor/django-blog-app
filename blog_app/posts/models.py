706883ce341674c10367114875532e5525205a14
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-08-30 18:00:00 -0500

    Define post model initial parameters to meet test cases

--- blog_app/posts/models.py
+++ blog_app/posts/models.py
@@ -3,3 +3,9 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+  title = models.CharField(max_length=100)  
+  content = models.TextField()
+  comments_count = models.IntegerField()
+  likes_count = models.IntegerField()
+  create_date = models.DateField()
+  update_date = models.DateField()

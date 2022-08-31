f5d8725d236b44b9582565dcc791b04852fe7cac
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-08-30 20:45:03 -0500

    Add validations to meet test cases

--- blog_app/posts/models.py
+++ blog_app/posts/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
 class Post(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
-  title = models.CharField(max_length=100)  
+  title = models.CharField(max_length=40, validators = [MinLengthValidator(limit_value= 5)])  
   content = models.TextField()
   comments_count = models.IntegerField()
   likes_count = models.IntegerField()

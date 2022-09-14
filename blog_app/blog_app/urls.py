401dc68d7523652a6074a6d32a8977d858232892
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-09-14 12:15:47 -0500

    Define config to use base template directory
    - Define url setting to use posts index as home

--- blog_app/blog_app/urls.py
+++ blog_app/blog_app/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls'))
+    path('posts/', include('posts.urls')),
+    path('', views.index, name='home')
 ]
